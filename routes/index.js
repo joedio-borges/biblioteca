515c4b2ca803dfa3327aff42939da5b57cbb04dc
Joedio Borges Junior <joedio.borges@ifc.edu.br>
2022-07-21 13:11:18 -0300

    etapa 4

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/sol', function(req, res, next) {
-  res.send('Estamos no verão!');
+// GET home page.
+router.get('/', function(req, res) {
+  res.redirect('/catalog');
 });
 
 module.exports = router;
